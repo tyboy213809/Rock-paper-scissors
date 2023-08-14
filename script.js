e2410635d79781e63245f11ac270a095c6601cd0
tyboy213809 <31315062+tyboy213809@users.noreply.github.com>
2023-08-13 22:54:38 -0400

    got prompt working thinking of a way to check what they put is right

---
+++ script.js
@@ -0,0 +1,12 @@
+// human input 
+let human = prompt("rock, paper or scissors?");
+// check if human put the right thing 
+let Lhuman = human.toLowerCase();
+function check(checker){
+
+}
+// function get computerchoice randomly chooses rock paper scissors
+// compare the two which one wins 
+// count who won 
+
+console.log(Lhuman);
