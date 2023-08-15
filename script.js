74f8f8071f025f259c5388b6a15bc3e5a3de2f9f
tyboy213809 <31315062+tyboy213809@users.noreply.github.com>
2023-08-14 22:10:47 -0400

    finish on the computer randomaly choosing which one would win working on a function on who would win

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // human input 
 let human = prompt("rock, paper or scissors?");
 // check if human put the right thing 
-let Lhuman = human.toLowerCase();
+let Lhuman = human.toLowerCase().trim();
 function check(checker){
+    if (checker === "rock" || checker === "paper" || checker === "scissors"){
+        console.log("true")
+    }else {
+        return "entered wrong choice";
+    }
+} 
 
-}
+let result = check(Lhuman);
+console.log(result);
+
 // function get computerchoice randomly chooses rock paper scissors
 // compare the two which one wins 
 // count who won 
-
-console.log(Lhuman);
